76e042a1928722cf5763592d4793ff9593b96773
Yoshihiro Seki <nikochan2k@gmail.com>
2021-09-24 01:23:45 +0900

    mainを追加

--- city-walker-cli/src/main/java/nikochan2k/citywalker/Cli.java
+++ city-walker-cli/src/main/java/nikochan2k/citywalker/Cli.java
@@ -13,6 +13,7 @@ import org.reflections.Reflections;
 
 import com.esotericsoftware.wildcard.Paths;
 
+import picocli.CommandLine;
 import picocli.CommandLine.Command;
 import picocli.CommandLine.Option;
 import picocli.CommandLine.Parameters;
@@ -131,5 +132,9 @@ public class Cli implements Callable<Integer> {
 		}
 		return factories;
 	}
-
+	
+    public static void main(String... args) {
+        int exitCode = new CommandLine(new Cli()).execute(args);
+        System.exit(exitCode);
+    }
 }
